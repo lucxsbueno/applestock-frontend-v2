f863720c0676e806113e37aa725ce568a348f119
lucxsbueno <lucas_bueno2@estudante.sc.senai.br>
2025-07-18 20:44:42 -0300

    feat: add initial layout and page for dashboard

---
+++ components/dashboard/dashboard-section.tsx
@@ -0,0 +1,9 @@
+import { ModeToggle } from "@/components/mode-toggle";
+
+export function DashboardSection() {
+  return (
+    <div className="w-full h-dvh bg-white dark:bg-zinc-900">
+      <ModeToggle />
+    </div>
+  );
+}
